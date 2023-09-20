7c3338c52eb980cd963d374f34a35567768e826e
SK <sunil.kafle@hotmail.com>
2023-09-20 13:59:16 -0400

    code and readme changed

--- OOP_README.md
+++ OOP_README.md
@@ -3,4 +3,4 @@
 * class variables are shared among all instances of the class.
 * instance variables unique to each instances e.g. name, pay, email etc.
 * class variable should be same for the each instances.
-* annual raise >> class variable as all instances have same raise.
+* annual raise >> class variable as all instances(individual employee) have same raise.
--- oop1_class_variables.py
+++ oop1_class_variables.py
@@ -1,20 +1,40 @@
-#  OOP in Python
+#  OOP in Python https://www.youtube.com/watch?v=BJ-VvGyQxho
 class Employee:
+    num_of_emps = 0
+    raise_amount = 1.04  # class variable
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + "." + last + "@company.com"
-
+        Employee.num_of_emps +=1
     def fullname(self):
         return self.first + " " + self.last
     # OR using .format:  return "{} {}".format(self.first, self.last)
 
+    # create new instance variable which adds the raised amount to original pay.
+    def apply_raise(self):
+        # self.pay = int(self.pay * 1.04) # without class variable
+        self.pay = int(self.pay * self.raise_amount) # with class variable need to specify self or Emplyee in the begining
+
+print(Employee.num_of_emps)
 emp_1 = Employee("Tom", "Cruz", 90000)
 emp_2 = Employee("Michael","Jordan", 100000)
+#print the attributes
+print(Employee.__dict__)
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+
+print(Employee.num_of_emps)
 
 
 
-print(emp_1.fullname())
-print(Employee.fullname(emp_2))
-print(emp_1.email)
+# print(emp_1.fullname())
+# print(Employee.fullname(emp_2))
+# print(emp_1.email)
