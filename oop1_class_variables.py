479ea16bf2bb8835f3f0c330e6980098c4639807
SK <sunil.kafle@hotmail.com>
2023-09-20 13:23:58 -0400

    File changed

--- oop1_class_variables.py
+++ oop1_class_variables.py
@@ -8,8 +8,13 @@ class Employee:
 
     def fullname(self):
         return self.first + " " + self.last
+    # OR using .format:  return "{} {}".format(self.first, self.last)
+
 emp_1 = Employee("Tom", "Cruz", 90000)
 emp_2 = Employee("Michael","Jordan", 100000)
+
+
+
 print(emp_1.fullname())
 print(Employee.fullname(emp_2))
 print(emp_1.email)
