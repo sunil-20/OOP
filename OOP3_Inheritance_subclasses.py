6b908ff0a862859e54dda565ba5d85dd8a44ce73
SK <sunil.kafle@hotmail.com>
2023-09-20 23:33:48 -0400

    code changed

--- OOP3_Inheritance_subclasses.py
+++ OOP3_Inheritance_subclasses.py
@@ -24,13 +24,36 @@ class Developer(Employee):
         self.prog_lang = prog_lang
 
 
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees = None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+    # printing the names of the emplyees full name
+    def print_emps(self):
+        for emp in self.employees:
+            print("-->", emp.fullname())
+
+
+
+
 
 dev_1 = Developer("John", "Deere", 20000, "Python")
 dev_2 = Developer("Michael", "Jordan", 30000, "Java")
+mgr_1 = Manager("Sue", "Morgan", 900000, [dev_1])
 
+print(mgr_1.email)
 #print(help(Developer))
 # print(dev_1.email)
 # print(dev_2.email)
-print(dev_1.email)
-#dev_1.apply_raise()
-print(dev_1.prog_lang)
+# print(dev_1.email)
+# #dev_1.apply_raise()
+# print(dev_1.prog_lang)
