72a4934efb9787cb22f496df816f5be35f1e2495
SK <sunil.kafle@hotmail.com>
2023-09-20 22:29:16 -0400

    code changed

--- OOP2_class_and_staticmethod.py
+++ OOP2_class_and_staticmethod.py
@@ -13,8 +13,11 @@ class Employee:
 
     def fullname(self):
         return "{} {}".format(self.first, self.last)
+    
+
     def apply_raise(self):
         self.pay = int(self.pay * 1.04)
+
     # creating classmethod
     @classmethod 
     # common convention to use cls as self in class
@@ -25,6 +28,12 @@ class Employee:
         #split the string 
         first, last, pay = emp_str_1.split("-")
         return cls(first, last, pay)
+    # staticmethods
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 # changing the raise amount to 5 % using the class method
 Employee.set_raise_amt(1.05)
@@ -32,15 +41,20 @@ Employee.set_raise_amt(1.05)
 emp_1 = Employee("Tom", "Cruz", 2000000)
 emp_2 = Employee("Tim", "Lee", 400000)
 
-emp_str_1 = "John-Doe-70000"
-emp_str_2 = "Steve-Smith-30000"
-emp_str_3 = "Jane-Doe-90000"
+import datetime
+my_date = datetime.date(2016, 7, 10)
+print(Employee.is_workday(my_date))
 
-new_emp_1 = Employee.from_string(emp_str_1) # From_string alternative constructor
-print(new_emp_1.email)
-print(new_emp_1.pay)
+# emp_str_1 = "John-Doe-70000"
+# emp_str_2 = "Steve-Smith-30000"
+# emp_str_3 = "Jane-Doe-90000"
+
+# new_emp_1 = Employee.from_string(emp_str_1) # From_string alternative constructor
+# print(new_emp_1.email)
+# print(new_emp_1.pay)
 
 
+    
 
 # print(Employee.raise_amt)
 # print(emp_1.raise_amt)
