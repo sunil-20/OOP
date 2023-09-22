52d4a781d0a158e493b54eaaaed060b44cfced61
SK <sunil.kafle@hotmail.com>
2023-09-22 13:53:30 -0400

    code changed

--- OOP4_Special_Methods.py
+++ OOP4_Special_Methods.py
@@ -24,10 +24,20 @@ class Employee:
 # it escapes _repr_ method
     def __str__(self):
         return "{} - {}".format(self.fullname(), self.email)
+    def __add__(self, other):
+        return self.pay + other.pay
+    def __len__(self):
+        return len(self.fullname())
 
 
 emp_1 = Employee("Tom", "Cruz", 20000)
 emp_2 = Employee("Jackie", "Chen", 30000)
+
+# string length
+print(len("test"))
+print("test".__len__())
+print(len(emp_1))
+print(len(emp_1.fullname()))
 
 #print(emp_1) # this prints the str method only irrespective of repr
 
@@ -35,9 +45,13 @@ emp_2 = Employee("Jackie", "Chen", 30000)
 # print(repr(emp_1)) # printing employee instance whithout object info..
 # print(str(emp_1))
 
-print(emp_1.__repr__())
-print(emp_1.__str__())
+# print(emp_1.__repr__())
+# print(emp_1.__str__())
 
-# dunder method for addition int and str objects
-print(int.__add__(1,2))
-print(str.__add__("a", "b"))
+# # dunder method for addition int and str 
+# # https://docs.python.org/3/reference/datamodel.html#special-method-names
+# print(int.__add__(1,2))
+# print(str.__add__("a", "b"))
+
+# print using the __add__ dunder method
+#print( emp_1 + emp_2)
